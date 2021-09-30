ca95366b3516ef616562e1aced5902a35865a06b
Claudius <58847225+cruecker@users.noreply.github.com>
2021-09-30 15:18:01 +0200

    Update Get-ArchiveStats.ps1

--- exchange-scripts/M365Migration/Get-ArchiveStats.ps1
+++ exchange-scripts/M365Migration/Get-ArchiveStats.ps1
@@ -1,3 +1,3 @@
 #Messen der Grösse der Archive und Itemanzahl und export in eine CSV
 $datum = Get-Date -Format "ddMMyyyyHHmm"
-Get-Mailbox -resultsize unlimited -Archive | Select-Object name,RecipientTypeDetails,@{n="Primary Size";e={(Get-MailboxStatistics $_.identity -Archive).totalItemsize}},@{n="Primary Item Count";e={(Get-MailboxStatistics $_.identity -Archive).ItemCount}} | export-csv -NoTypeInformation -Encoding UTF8 -Delimiter "," -Path C:\Temp\Claudius\Archivstats$Datum.txt
+Get-Mailbox -resultsize unlimited -Archive | Select-Object name,RecipientTypeDetails,@{n="Primary Size";e={(Get-MailboxStatistics $_.identity -Archive).totalItemsize}},@{n="Primary Item Count";e={(Get-MailboxStatistics $_.identity -Archive).ItemCount}} | export-csv -NoTypeInformation -Encoding UTF8 -Delimiter "," -Path C:\Temp\Archivstats$Datum.txt
