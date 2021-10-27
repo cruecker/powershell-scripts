1e79e9a085c84e273509b32ebe6efbc6e547615e
Claudius <58847225+cruecker@users.noreply.github.com>
2021-10-27 08:26:35 +0200

    Update get-EmailAddressExport.ps1

--- exchange-scripts/M365Migration/get-EmailAddressExport.ps1
+++ exchange-scripts/M365Migration/get-EmailAddressExport.ps1
@@ -3,5 +3,5 @@ $FormatEnumerationLimit =-1
 $datum = Get-Date -Format "ddMMyyyyHHmm"
 $foldertosave = "C:\Temp\Claudius\email-addresses-export_$datum.txt"
 
-#Abfrage
-get-mailbox -ResultSize unlimited | ft name, alias, emailaddresses -wrap | out-file $foldertosave
+#Abfrage und export
+get-mailbox -ResultSize unlimited | select name, alias, @{Name='EmailAddresses'; Expression={$_.EmailAddresses -join ","}} | export-csv $foldertosave -NoTypeInformation -Delimiter ";" -Encoding "UTF8"
